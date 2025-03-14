faf1138de2a23a5dcced4871265a5a0ecebebac2
zjf <meetsviva@163.com>
2025-03-14 21:02:50 +0800

    初步的后端接口

--- application/topo/FatTree6/receive.py
+++ application/topo/FatTree6/receive.py
@@ -41,108 +41,120 @@ bytes = 0
 check_bytes = 0
 end_time = time.time()
 topo = Topology('topology.json')
-#rules = load_rules('./topo/FatTree/rules.json')
-rules = load_rules('rules.json')
-bug_num = 0
+# rules = load_rules('./topo/FatTree/rules.json')
+rules = load_rules('topo/FatTree6/rules.json')
+# bug_num = 0
 
-# print(rules)
 
 def verification(pkt: Packet):
-    global bug_num
+    # global bug_num
     # verification consistent
     src_ip, dst_ip, sport, dport, proto = get_5tuple(pkt)
     # print("src_ip: %s, dst_ip: %s, sport: %s, dport: %s, proto: %s" % (src_ip, dst_ip, sport, dport, proto))
     # print("rules: %s" %(rules))
     key = src_ip + '-' + dst_ip
-    if key not in rules:
-        print("not found src_ip: %s, dest_ip: %s in rules store." % (src_ip, dst_ip)) 
-        sys.exit()
-        
-    expected_path = rules[key]
-    print('source-destination pair: (%s, %s), expected_path: %s' % (src_ip, dst_ip, expected_path))
-    print(pkt)
-    if IPOption_TAG in pkt:
-        prime_prod = pkt['TAG'].prime_product
-        # 验证质数乘积是否正确
-        # TODO
-        expected_prod = 1
-        for node in expected_path:
-            prime = topo.get_prime('s' + str(node))
-            if prime == 1:
-                continue
-            expected_prod = expected_prod * prime
-            
-        if prime_prod != expected_prod:
-            print('Inconsistent = source-destination pair: (%s, %s), prime_prod: %s, expected_prod: %s,  time: %s' % (src_ip, dst_ip, prime_prod, expected_prod,  time.time()))
-            bug_num = bug_num + 1
-            if bug_num == 7:
-                sys.exit()
-        
-            # 故障定位
-            tmp_prod = prime_prod
-            pre_switch = None
-            end_switch = None
-            flag = False
-            path=[]
-            # 确定尾部交换机
-            for node in topo.get_neighbors(topo.get_id(dst_ip)):
-                tmp_prime = topo.get_prime(node)
-                if tmp_prod % tmp_prime == 0:
-                    end_switch = node
-                    tmp_prod /= tmp_prime
-                    flag = True
-                    break
 
-            if end_switch != None:
-                while tmp_prod > 1:
-                    flag = False
-                    path.append(end_switch)
-                    for node in topo.get_neighbors(end_switch):
-                        if pre_switch == None or node != pre_switch:
-                            prime = topo.get_prime(node)
-                            if prime == 1:
-                                continue
-                            if tmp_prod % prime == 0:
-                                pre_switch = end_switch
-                                end_switch = node
-                                tmp_prod /= prime
-                                flag = True
-                                break
-                        
-            if flag :
-                print("recover success! prime_prod: %s, forward path: %s"% (prime_prod, path[::-1]))
-            else:
-                print("Error: can't recover th actual forward path. prime product: %s" % (prime_prod))
-                sys.exit()
+    with open("res.json", "w")as f:
+        res = {
+            "src_ip":src_ip,
+            "dst_ip":dst_ip,
+            "expected_path":None,
+            "consistence":True,
+            "recover_path":None
+            }
+        if key not in rules:
+            print("not found src_ip: %s, dest_ip: %s in rules store.\n" % (src_ip, dst_ip))
+            sys.exit()
             
-    elif IPOption_MRI in pkt:
-        count = pkt['MRI'].count - 1
-        path = []
-        while (count >= 0):
-            swid = pkt['MRI'].swtraces[count].swid
-            inport = pkt['MRI'].swtraces[count].inport
-            outport = pkt['MRI'].swtraces[count].outport
-            
-            # [swid, inport, outport]
-            path.append(swid)
-            count = count - 1
-        
-        found = False
-        if len(path) != len(expected_path):
-            found = True
-        else:
-            i = 0
-            while i < len(path):
-                if path[i] != expected_path[i]:
-                    found = True
-                    break
-                i = i + 1
+        expected_path = rules[key]
+        res['expected_path'] = expected_path
+        print('source-destination pair: (%s, %s), expected_path: %s\n' % (src_ip, dst_ip, expected_path))
+        if IPOption_TAG in pkt:
+            prime_prod = pkt['TAG'].prime_product
+            # 验证质数乘积是否正确
+            # TODO
+            expected_prod = 1
+            for node in expected_path:
+                prime = topo.get_prime('s' + str(node))
+                if prime == 1:
+                    continue
+                expected_prod = expected_prod * prime
                 
-        if found:
-            print('Inconsistent = source-destination pair: (%s, %s), path: %s, expected_path: %s, time: %s' % (src_ip, dst_ip, path, expected_path, time.time()))
-            bug_num = bug_num + 1
-            if bug_num == 7:
-                sys.exit()
+            if prime_prod != expected_prod:
+                print('Inconsistent = source-destination pair: (%s, %s), prime_prod: %s, expected_prod: %s,  time: %s\n' % (src_ip, dst_ip, prime_prod, expected_prod,  time.time()))
+                res['consistence'] = False
+                # bug_num = bug_num + 1
+                # if bug_num == 7:
+                #     sys.exit()
+            
+                # 故障定位
+                tmp_prod = prime_prod
+                pre_switch = None
+                end_switch = None
+                flag = False
+                path=[]
+                # 确定尾部交换机
+                for node in topo.get_neighbors(topo.get_id(dst_ip)):
+                    tmp_prime = topo.get_prime(node)
+                    if tmp_prod % tmp_prime == 0:
+                        end_switch = node
+                        tmp_prod /= tmp_prime
+                        flag = True
+                        break
+
+                if end_switch != None:
+                    while tmp_prod > 1:
+                        flag = False
+                        path.append(int(end_switch[1:]))
+                        for node in topo.get_neighbors(end_switch):
+                            if pre_switch == None or node != pre_switch:
+                                prime = topo.get_prime(node)
+                                if prime == 1:
+                                    continue
+                                if tmp_prod % prime == 0:
+                                    pre_switch = end_switch
+                                    end_switch = node
+                                    tmp_prod /= prime
+                                    flag = True
+                                    break
+                            
+                if flag :
+                    print("recover success! prime_prod: %s, forward path: %s\n"% (prime_prod, path[::-1]))
+                    res['recover_path'] = path[::-1]
+                else:
+                    print("Error: can't recover th actual forward path. prime product: %s\n" % (prime_prod))
+                    sys.exit()
+                
+        elif IPOption_MRI in pkt:
+            count = pkt['MRI'].count - 1
+            path = []
+            while (count >= 0):
+                swid = pkt['MRI'].swtraces[count].swid
+                inport = pkt['MRI'].swtraces[count].inport
+                outport = pkt['MRI'].swtraces[count].outport
+                
+                # [swid, inport, outport]
+                path.append(swid)
+                count = count - 1
+            
+            found = False
+            if len(path) != len(expected_path):
+                found = True
+            else:
+                i = 0
+                while i < len(path):
+                    if path[i] != expected_path[i]:
+                        found = True
+                        break
+                    i = i + 1
+                    
+            if found:
+                print('Inconsistent = source-destination pair: (%s, %s), path: %s, expected_path: %s, time: %s' % (src_ip, dst_ip, path, expected_path, time.time()))
+                bug_num = bug_num + 1
+                if bug_num == 7:
+                    sys.exit()
+    
+        json.dump(res, f, indent=4)
         
 def handle_pkt(pkt: Packet):
     global NUM
